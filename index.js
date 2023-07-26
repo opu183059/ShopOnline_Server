8163f2b9c914eead97b986f13e00b887037ef33a
opu183059 <opuctg440@gmail.com>
2023-07-26 21:22:49 +0600

    get all products added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const app = express(); //
+const app = express();
+require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
@@ -7,6 +8,43 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ljsyrma.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const productDB = client.db("ShopOnline").collection("productDB");
+
+    // Get all Products
+    app.get("/allProducts", async (req, res) => {
+      const result = await productDB.find({}).toArray();
+      res.json(result);
+    });
+    // --------------------------------------------------------------------------------------------------------
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// Server running output message
 app.get("/", (req, res) => {
   res.send("ShopOnline server is running");
 });
